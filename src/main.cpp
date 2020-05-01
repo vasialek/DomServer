0d547c8dec9bca1db76f77e48d347f0a8e606948
Anton <antoha.c2013@yandex.ru>
2020-05-01 11:56:51 +0300

    added ldr

--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,24 @@
 #endif
 
 // SSID and PW for Config Portal
-String ssid = "";
-String password = "";
+String ssid = "myWiFi";
+String password = "red";
 
 #include <ESP_WiFiManager.h>              //https://github.com/khoih-prog/ESP_WiFiManager
 
-const int PinRed = 15;
-const int PinBlue = 13;
-const int PinGreen = 12;
+// const int PinRed = 15;
+// const int PinBlue = 13;
+// const int PinGreen = 12;
+const int ledPin = 12;
+const int ldrPin = A0;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Starting configuration portal...");
 
-  pinMode(PinBlue, OUTPUT);
-  pinMode(PinGreen, OUTPUT);
-  pinMode(PinRed, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ldrPin, INPUT);
+  
 
   ESP_WiFiManager wifiManager("Wemos-Dev");
   
@@ -41,27 +43,30 @@ void setup() {
     wifiManager.setConfigPortalTimeout(30);
   }
   
-  wifiManager.autoConnect("Wemos_AutoConnectAP", "");
+  wifiManager.autoConnect("myWiFi", "red");
 }
 
 void printHeartBeat()
 {
+  int ldrStatus = analogRead(ldrPin);
+
   static bool isLedOn = false;
   Serial.print("WiFi status: ");
+  Serial.println(ldrStatus);
   Serial.println(WiFi.status() == WL_CONNECTED ? "connected" : "not connected");
   if (isLedOn)
   {
     if (WiFi.status() == WL_CONNECTED)
     {
-      analogWrite(PinBlue, 150);
+      analogWrite(ledPin, 250);
     } else
     {
-      analogWrite(PinRed, 150);
+      analogWrite(ledPin, 150);
     }
   }else
   {
-    analogWrite(PinBlue, 0);
-    analogWrite(PinRed, 0);
+    analogWrite(ledPin, 0);
+    analogWrite(ledPin, 0);
   }
   
   isLedOn = !isLedOn;
