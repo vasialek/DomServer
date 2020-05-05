0ddc7591df00626b7dab5613129909ddd275dae4
Aleksej V <proglamer@gmail.com>
2020-05-05 23:49:44 +0300

    Use Asyn web server instead of WiFI manager.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,16 @@
 #include <Arduino.h>
 #ifdef ESP32
-  #include <esp_wifi.h>
   #include <WiFi.h>
-  #include <WiFiClient.h>
-#else
-  #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
-  //needed for library
-  #include <DNSServer.h>
-  #include <ESP8266WebServer.h>
+  #include <AsyncTCP.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <ESPAsyncTCP.h>
 #endif
+#include <ESPAsyncWebServer.h>
 
-// SSID and PW for Config Portal
-String ssid = "myWiFi";
-String password = "red";
-
-#include <ESP_WiFiManager.h>              //https://github.com/khoih-prog/ESP_WiFiManager
+// SSID and password for connection
+String ssid = "ML_16";
+String password = "XXXXXXXXXX";
 
 // const int PinRed = 15;
 // const int PinBlue = 13;
@@ -22,66 +18,99 @@ String password = "red";
 const int ledPin = 12;
 const int ldrPin = A0;
 
+// Possible WiFi states
+const int WiFiClientInitializing = 0;
+const int WiFiClientConnecting = 1;
+const int WiFiClientConnected = 2;
+const int WiFiAccessPoint = 4;
+const int WiFiSetup = 8;
+const int WiFiServing = 16;
+
+const ulong HeartbeatTimeoutMs = 2000;
+
+int wifiState = WiFiClientInitializing;
+
+AsyncWebServer server(80);
+
+void handleNotFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Page not found");
+}
+
+void handleIndex(AsyncWebServerRequest *request) {
+  request->send(200, "text/html", "<html><head><title>DomServer</title></head><body>Welcome to DomServer</body></html>");
+}
+
+void handleLdr(AsyncWebServerRequest *request) {
+  request->send(200, "text/plain", "LDR: " + String(analogRead(ldrPin)));
+}
+
 void setup() {
   Serial.begin(9600);
-  Serial.println("Starting configuration portal...");
+  Serial.println("Starting DomServer...");
 
   pinMode(ledPin, OUTPUT);
   pinMode(ldrPin, INPUT);
-  
-
-  ESP_WiFiManager wifiManager("Wemos-Dev");
-  
-  wifiManager.setDebugOutput(true);
-
-  ssid = wifiManager.WiFi_SSID();
-  password = wifiManager.WiFi_Pass();
-  Serial.println("Stored: SSID = " + ssid + ", password = " + password);
-
-  if (ssid != "")
-  {
-    wifiManager.setConfigPortalTimeout(30);
-  }
-  
-  wifiManager.autoConnect("myWiFi", "red");
 }
 
 void printHeartBeat()
 {
-  int ldrStatus = analogRead(ldrPin);
-
-  static bool isLedOn = false;
+  Serial.print("State of WiFi client: ");
+  Serial.println(wifiState);
   Serial.print("WiFi status: ");
-  Serial.println(WiFi.status());
-  Serial.print("LDR value: ");
-  Serial.println(ldrStatus);
   Serial.println(WiFi.status() == WL_CONNECTED ? "connected" : "not connected");
-  if (isLedOn)
+  if (WiFi.status() == WL_CONNECTED)
   {
-    if (WiFi.status() == WL_CONNECTED)
-    {
-      analogWrite(ledPin, 250);
-    } else
-    {
-      analogWrite(ledPin, 150);
-    }
-  }else
-  {
-    analogWrite(ledPin, 0);
-    analogWrite(ledPin, 0);
+    Serial.print("IP of DomServer: ");
+    Serial.println(WiFi.localIP());
   }
-  
-  isLedOn = !isLedOn;
-  
-  
+}
+
+void setupServerHandlers() {
+  server.on("/", HTTP_GET, handleIndex);
+  server.on("/ldr", HTTP_GET, handleLdr);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
 }
 
 void loop() {
+  static ulong wifiConnectionTimeoutMs = 0;
   static ulong checkstatusTimeoutMs = 0;
+
+  switch (wifiState)
+  {
+  case WiFiClientInitializing:
+    Serial.print("Going to connect to WiFi: ");
+    Serial.print(ssid.c_str());
+    Serial.print("/");
+    Serial.println(password.c_str());
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid.c_str(), password.c_str());
+    wifiState = WiFiClientConnecting;
+    break;
+
+  case WiFiClientConnecting:
+    if (millis() > wifiConnectionTimeoutMs || wifiConnectionTimeoutMs == 0)
+    {
+      if (WiFi.status() == WL_CONNECTED)
+      {
+        wifiState = WiFiSetup;
+      }
+      wifiConnectionTimeoutMs += HeartbeatTimeoutMs;
+    }
+    break;
+
+    case WiFiSetup:
+      Serial.println("Going to start WEB server...");
+      setupServerHandlers();
+      wifiState = WiFiServing;
+      break;
+  }
+
   if (millis() > checkstatusTimeoutMs || checkstatusTimeoutMs == 0)
   {
     printHeartBeat();
-    checkstatusTimeoutMs += 1000;
+    checkstatusTimeoutMs += HeartbeatTimeoutMs;
   }
   
 }
