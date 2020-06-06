94e809a71ab61f942015cabdaa55adf894eb7bb1
Aleksej V <proglamer@gmail.com>
2020-06-06 20:41:51 +0300

    Credentials are moved into separate file.

---
+++ src/credentials.h
@@ -0,0 +1,8 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// SSID and password for connection
+const char *ssid = "*** Your SSID here ***";
+const char *password = "*** Your password here ***";
+
+#endif
